7c4d759394b44778374ef7a20be458e482696988
Priti Kumari <priti.kumari2910@gmail.com>
2019-06-05 14:46:35 +0530

    First Commit with main page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeContainerComponent } from './home-container/home-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeContainerComponent
   ],
   imports: [
     BrowserModule
