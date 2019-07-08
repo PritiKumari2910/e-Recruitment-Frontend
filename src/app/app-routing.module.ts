c1b8e0ff3a8d4a132000a0a95af3d1e1a56060bc
Priti Kumari <priti.kumari2910@gmail.com>
2019-07-08 11:12:49 +0530

    Routing from Login to welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 
 const routes: Routes = [
-  {path:'',component:LoginComponent},
+  //{path:'',component:LoginComponent},
   {path:'login',component:LoginComponent},
+  {path:'welcome/:name',component:WelcomeComponent, canActivate:[RouteGuardService]}
     
   //this should be at last 
   //{path:'**',component:ErrorComponent},
